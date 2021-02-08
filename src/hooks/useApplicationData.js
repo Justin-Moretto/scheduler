8eab4f02228483f4f1deba834ad1e28f3e8adb34
Justin-Moretto <JustinMorettoMTL@gmail.com>
2021-02-08 17:20:48 -0500

    added acces control header to useEffect get requests in useApplicationData.js

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -15,9 +15,12 @@ export default function useApplicationData() {
   useEffect(() => {
     console.log('GETTING STATES FROM API SERVER...')
     Promise.all([
-      axios.get('/api/days'),
-      axios.get('/api/appointments'),
-      axios.get('/api/interviewers')
+      axios.get('/api/days',
+      { headers: { 'Access-Control-Allow-Origin': '*' }),
+      axios.get('/api/appointments',
+      { headers: { 'Access-Control-Allow-Origin': '*' }),
+      axios.get('/api/interviewers',
+      { headers: { 'Access-Control-Allow-Origin': '*' })
     ]).then((all) => {
       setState(prev => ({...prev, days: all[0].data,
         appointments: all[1].data,
